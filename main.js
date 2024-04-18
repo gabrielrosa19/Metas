0829c744e4bbc2f5b196d974d7460f1678f2a046
Gabriel15 <105285083+gabrielrosa19@users.noreply.github.com>
2024-04-18 10:30:22 -0300

    ok

--- main.js
+++ main.js
@@ -36,7 +36,6 @@ function calculaTempo(tempoObjetivo) {
         if (tempoFinal > 0){
             return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
         }else{
-            return " PRAZO ENCERRADO!!! "
         }
 }
 
